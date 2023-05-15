2ea318daa377cc6b1e9f0f53f5aeca83938e51aa
wihyanghoon <gidgns1995@gmail.com>
2023-05-15 23:59:55 +0900

    fetch

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const http = require('http');
 const fs = require('fs').promises
 
 const server = http.createServer(async(req, res)=>{
-  const data = await fs.readFile('./mainhtml')
+  if(req.url === '/'){
+    if(req.method === "GET") {
+      const data = await fs.readFile('./mainhtml')
+      res.end(data)
+    }
+  }
+  if(req.url === '/'){
+    if(req.method === "GET") {
+      const data = await fs.readFile('./mainhtml')
+      res.end(data)
+    }
+  }
+  
 }).listen(8080)
 
 
 server.on('listening', ()=>{
-  console.log('서버가 연결되었습니다.')
+  console.log('서버가 연결되었습니다')
 })
 
 server.on('error', (error)=>{
