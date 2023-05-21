9a5deb199b0d28d1a489190c60766ab58610a5d8
wihyanghoon <gidgns1995@gmail.com>
2023-05-21 23:01:54 +0900

    fetch

--- main.js
+++ main.js
@@ -1,25 +1,11 @@
-const express = require("express");
-const path = require('path')
-const morgan = require("morgan");
+const http = require('http')
 
-const app = express();
-const loggar = morgan("dev");
+http.createServer(async(req, res)=>{
+  res.write('<h1>Hello</h1>')
 
-app.use(loggar);
-app.set('port', process.env.PORT || 8080)
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, './main.html'))
-});
-
-app.get("/post", (req ,res) => {
-  res.send('post do it')
+  res.end()
+}).listen(8080, () => {
+  console.log('fdfd')
 })
 
-app.get("/catagory/:name", (req, res) => {
-  res.send(`hello ${req.params.name}`)
-})
-
-app.listen(app.get('port'), () => {
-  console.log("서버실행중");
-});
+for
